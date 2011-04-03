77bd75e34ce20bb75f027c8f219f50a7610d537c
Andrew Kelley <superjoe30@gmail.com>
2011-04-03 00:24:24 -0700

    can draw in level editor but can't save

--- lemming/data.py
+++ lemming/data.py
@@ -1,12 +1,13 @@
-'''Simple data loader module.
+"""
+Simple data loader module.
 
-Loads data files from the "data" directory shipped with a game.
+Loads data files from the 'data' directory shipped with a game.
 
 Enhancing this to handle caching etc. is left as an exercise for the reader.
 
 Note that pyglet users should probably just add the data directory to the
 pyglet.resource search path.
-'''
+"""
 
 from __future__ import division, print_function, unicode_literals
 range = xrange
@@ -16,14 +17,14 @@ data_py = os.path.abspath(os.path.dirname(__file__))
 data_dir = os.path.normpath(os.path.join(data_py, '..', 'data'))
 
 def filepath(filename):
-    '''Determine the path to a file in the data directory.
-    '''
+    "Determine the path to a file in the data directory."
     return os.path.join(data_dir, filename)
 
 def load(filename, mode='rb'):
-    '''Open a file in the data directory.
+    """
+    Open a file in the data directory.
 
-    "mode" is passed as the second arg to open().
-    '''
+    'mode' is passed as the second arg to open().
+    """
     return open(os.path.join(data_dir, filename), mode)
 
--- lemming/game.py
+++ lemming/game.py
@@ -1,76 +1,210 @@
 from __future__ import division, print_function, unicode_literals
 range = xrange
 
-import pyglet
 import tiles
+import data
+from vec2d import Vec2d
 
+import pyglet
 import sys
-
-target_fps = 60
+import pickle
 
 class Game:
+    target_fps = 60
+    tile_size = (tiles.width, tiles.height)
+
+    class Mode:
+        NORMAL = 0
+        CHOOSE_TILE = 1
+
+    def load_images(self):
+        tileset = pyglet.image.load('tiles', file=data.load("tiles.bmp"))
+        for tile in tiles.info.values():
+            tile['image'] = tileset.get_region(tile['x']*tiles.width, tile['y']*tiles.height, tiles.width, tiles.height)
+
     def __init__(self):
-        self.level = None
-        self.grid_on = None
-        self.scroll_x = None
-        self.scroll_y = None
-        self.zoom = None
+        self.load_images()
+        self.clear_level()
 
     def clear_level(self):
-        # level is a 2d grid of cells. [y][x]
-        self.level = []
+        self.level = {
+            'start': {'x': 0, 'y': 0},
+            'tiles': {}, # (x, y) to {'tile'}
+        }
         self.grid_on = True
-        self.scroll_x = 0
-        self.scroll_y = 0
+        self.scroll = Vec2d(0, 0)
         self.zoom = 1
+        self.level_filename = None
+        self.mode = Game.Mode.NORMAL
+
+        self.left_mouse_down = False
+        self.right_mouse_down = False
+        self.selected_tile = tiles.info.iteritems().next()[0]
 
     def update(self, dt):
         pass
 
     def on_draw(self):
         self.window.clear()
-        if self.grid_on:
-            # draw grid
-            grid_color = (255, 255, 255, 255)
-            coords = []
-            x_tile_count = int(self.window.width / tiles.width)
-            y_tile_count = int(self.window.width / tiles.height)
-            for x in range(x_tile_count):
-                coords += [x*tiles.width, 0, x*tiles.width, self.window.height]
-            for y in range(y_tile_count):
-                coords += [0, y*tiles.height, self.window.width, y*tiles.height]
-            pyglet.graphics.draw(int(len(coords)/2), pyglet.gl.GL_LINES, ('v2i', coords), ('c4B', grid_color * int(len(coords)/2)))
+
+        if self.mode == Game.Mode.NORMAL:
+            if self.grid_on:
+                # draw grid
+                grid_color = (255, 255, 255, 255)
+                coords = []
+                x_tile_count = int(self.window.width / tiles.width)
+                y_tile_count = int(self.window.height / tiles.height)
+                for x in range(x_tile_count):
+                    draw_x = int((x*tiles.width - self.scroll.x % tiles.width) * self.zoom)
+                    coords += [draw_x, 0, draw_x, self.window.height]
+                for y in range(y_tile_count):
+                    draw_y = int((y*tiles.height - self.scroll.y % tiles.height) * self.zoom)
+                    coords += [0, draw_y, self.window.width, draw_y]
+                pyglet.graphics.draw(int(len(coords)/2), pyglet.gl.GL_LINES, ('v2i', coords), ('c4B', grid_color * int(len(coords)/2)))
+
+            # draw tiles
+            start = self.abs_pt(Vec2d(0, 0)) / (tiles.width, tiles.height)
+            end = self.abs_pt(Vec2d(self.window.width, self.window.height)) / Game.tile_size
+            start.floor()
+            end.floor()
+            it = Vec2d(0, 0)
+            for it.y in range(start.y, end.y):
+                for it.x in range(start.x, end.x):
+                    rel = self.rel_pt(it * Game.tile_size).floored()
+                    tile = self.getTile(it)
+                    if tile['id'] != 0:
+                        self.getTile(it)['image'].blit(*rel)
+
+            # draw selected tile
+            if self.selected_tile is not None:
+                tiles.info[self.selected_tile]['image'].blit(0, self.window.height - tiles.height)
+
+        elif self.mode == Game.Mode.CHOOSE_TILE:
+            palette = self.getTilePalette()
+            for pos, tile in palette:
+                tile['image'].blit(pos.x, pos.y)
+
+    def getTile(self, pos):
+        level_tiles = self.level['tiles']
+        try:
+            tile_id = level_tiles[tuple(pos.floored())]['tile']
+        except KeyError:
+            return tiles.info[0]
+        return tiles.info[tile_id]
+
+    def setTile(self, pos, tile_id):
+        try:
+            self.level['tiles'][tuple(pos.floored())]['tile'] = tile_id
+        except KeyError:
+            self.level['tiles'][tuple(pos.floored())] = {'tile': tile_id}
+
+    def getTilePalette(self):
+        x = 0
+        max_x = self.window.width - tiles.width
+        y = self.window.height - tiles.height
+        for tile in tiles.info.values():
+            yield (Vec2d(x, y), tile)
+            x += tiles.width
+            if x >= max_x:
+                x = 0
+                y -= tiles.height
 
     def on_key_press(self, symbol, modifiers):
         if symbol == pyglet.window.key.G:
             self.grid_on = not self.grid_on
+        elif symbol == pyglet.window.key.LEFT:
+            self.scroll.x -= 100
+        elif symbol == pyglet.window.key.RIGHT:
+            self.scroll.x += 100
+        elif symbol == pyglet.window.key.UP:
+            self.scroll.y += 100
+        elif symbol == pyglet.window.key.DOWN:
+            self.scroll.y -= 100
+        elif symbol == pyglet.window.key.PLUS:
+            self.zoom *= 1.1
+        elif symbol == pyglet.window.key.MINUS:
+            self.zoom *= 0.9
+        elif symbol == pyglet.window.key._0:
+            self.zoom = 1
+        elif symbol == pyglet.window.key.SPACE:
+            self.mode = Game.Mode.CHOOSE_TILE
+
+    def on_key_release(self, symbol, modifiers):
+        if symbol == pyglet.window.key.SPACE:
+            self.mode = Game.Mode.NORMAL
+    def handle_mouse(self, pt):
+        tile_pos = self.abs_pt(pt) / Game.tile_size
+        if self.left_mouse_down:
+            self.setTile(tile_pos, self.selected_tile)
+        elif self.right_mouse_down:
+            self.setTile(tile_pos, 0)
 
     def on_mouse_motion(self, x, y, dx, dy):
-        pass
+        self.handle_mouse(Vec2d(x, y))
+
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        self.handle_mouse(Vec2d(x, y))
 
     def on_mouse_press(self, x, y, button, modifiers):
-        pass
+        if button & pyglet.window.mouse.LEFT:
+            self.left_mouse_down = True
+
+            if self.mode == Game.Mode.CHOOSE_TILE:
+                palette = self.getTilePalette()
+                for pos, tile in palette:
+                    if x >= pos.x and x <= pos.x + tiles.width and y >= pos.y and y <= pos.y + tiles.height:
+                        self.selected_tile = tile['id']
+                        print("Selected {0}".format(tile['name']))
+                        break
+        elif button & pyglet.window.mouse.RIGHT:
+            self.right_mouse_down = True
+        self.handle_mouse(Vec2d(x, y))
 
     def on_mouse_release(self, x, y, button, modifiers):
-        pass
+        if button & pyglet.window.mouse.LEFT:
+            self.left_mouse_down = False
+        elif button & pyglet.window.mouse.RIGHT:
+            self.right_mouse_down = False
 
-    def abs_pt(self, rel_pt_x, rel_pt_y):
-        return rel_pt_x * self.zoom + self.scroll_x, rel_pt_y * self.zoom + self.scroll_y
+    def abs_pt(self, rel_pt):
+        return rel_pt / self.zoom + self.scroll
 
-    def rel_pt(self, abs_pt_x, abs_pt_y):
-        return (abs_pt_x - self.scroll_y) / self.zoom, (abs_pt_y - self.scroll_y) / self.zoom
+    def rel_pt(self, abs_pt):
+        return (abs_pt - self.scroll) * self.zoom
 
-    def load_level(self, level_data):
-        pass
+    def load_level(self, filename):
+        self.level_filename = filename
+
+        pickle_error = False
+        try:
+            fd = open(filename, 'rb')
+            self.level = pickle.load(fd)
+            fd.close()
+        except pickle.UnpicklingError:
+            pickle_error = True
+        except IOError:
+            pickle_error = True
+            
+        if pickle_error:
+            print("Error loading level")
+            self.clear_level()
+            return
+
+    def saveLevel(self, filename):
+        fd = open(filename, 'wb')
+        pickle.dump(self.level, fd)
+        fd.close()
 
     def start(self):
-        self.window = pyglet.window.Window()
+        self.window = pyglet.window.Window(width=1152, height=648, vsync=False)
         self.window.set_handler('on_draw', self.on_draw)
         self.window.set_handler('on_key_press', self.on_key_press)
+        self.window.set_handler('on_key_release', self.on_key_release)
         self.window.set_handler('on_mouse_motion', self.on_mouse_motion)
         self.window.set_handler('on_mouse_press', self.on_mouse_press)
         self.window.set_handler('on_mouse_release', self.on_mouse_release)
-        pyglet.clock.schedule_interval(self.update, 1/target_fps)
+        self.window.set_handler('on_mouse_drag', self.on_mouse_drag)
+        pyglet.clock.schedule_interval(self.update, 1/Game.target_fps)
 
         self.parse_args()
 
@@ -86,15 +220,4 @@ class Game:
         if options_dict['level'] is not None:
             # load level from disk and go into level editor mode
             level_filename = options_dict['level']
-            level_data = None
-            try:
-                f = open(level_filename, 'rb')
-                level_data = f.read()
-                f.close()
-            except IOError:
-                # new level
-                pass
-            if level_data is not None:
-                self.load_level(level_data)
-            else:
-                self.clear_level()
+            self.load_level(level_filename)
