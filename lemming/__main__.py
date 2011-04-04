7034ed771334c3838a47136024990188620e4855
Andrew Kelley <superjoe30@gmail.com>
2011-04-04 02:16:25 -0700

    option to monkey patch pyglet to remove VBO's

--- lemming/__main__.py
+++ lemming/__main__.py
@@ -8,9 +8,19 @@ from leveleditor import LevelEditor
 def main():
     parser = OptionParser("see --help for options")
     parser.add_option("-l", "--level", dest="level", help="level")
+    parser.add_option('-n', '--novbo', action='store_true', help='Disable the use of VBOs (buggy/slow on some drivers)', default=False)
 
     options, args = parser.parse_args()
     options_dict = vars(options)
+
+    if options.novbo:
+        # monkey-patch pyglet (thanks muave http://code.google.com/p/bamboo-warrior/source/browse/trunk/run_game.py)
+        from pyglet.graphics import vertexdomain
+        default_create_attribute_usage = vertexdomain.create_attribute_usage
+        def create_attribute_usage(format):
+                attribute, usage, vbo = default_create_attribute_usage(format)
+                return attribute, usage, False
+        vertexdomain.create_attribute_usage = create_attribute_usage
     
     if options_dict['level'] is not None:
         # load level from disk and go into level editor mode
