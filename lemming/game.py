688c714428461dd06ec0e388c124f2153489ddd3
Andrew Kelley <superjoe30@gmail.com>
2011-04-03 20:23:50 -0700

    ability to detatch leader and give control to the next guy

--- lemming/game.py
+++ lemming/game.py
@@ -9,11 +9,22 @@ from level import Level
 import pyglet
 import sys
 
+def sign(n):
+    if n > 0:
+        return 1
+    elif n < 0:
+        return -1
+    else:
+        return 0
+
 class Game(object):
     class Control:
         MoveLeft = 0
         MoveRight = 1
         Jump = 2
+        BellyFlop = 3
+        Freeze = 4
+        Explode = 5
 
     class LemmingFrame(object):
         def __init__(self, pos, vel, next_node):
@@ -29,6 +40,7 @@ class Game(object):
         def __init__(self, sprite, frame=None):
             self.sprite = sprite
             self.frame = frame
+            self.gone = False
 
 
     target_fps = 60
@@ -62,6 +74,9 @@ class Game(object):
             pyglet.window.key.LEFT: Game.Control.MoveLeft,
             pyglet.window.key.RIGHT: Game.Control.MoveRight,
             pyglet.window.key.UP: Game.Control.Jump,
+            pyglet.window.key._1: Game.Control.BellyFlop,
+            pyglet.window.key._2: Game.Control.Freeze,
+            pyglet.window.key._3: Game.Control.Explode,
         }
 
     def __init__(self):
@@ -72,11 +87,17 @@ class Game(object):
     def clearLevel(self):
         self.level = Level(self.batch, self.group_level)
         self.scroll = Vec2d(0, 0)
+        self.scroll_vel = Vec2d(0, 0)
         self.zoom = 1
         self.lemmings = [None] * Game.lemming_count
         self.control_state = [False] * (len(dir(Game.Control)) - 2)
 
     def start(self):
+        self.control_lemming = 0
+        self.explode_queued = False # true when the user presses the button until an update happens
+        self.bellyflop_queued = False
+        self.freeze_queued = False
+
         # resets variables based on level and begins the game
         # generate data for each lemming
         for i in range(len(self.lemmings)):
@@ -98,72 +119,87 @@ class Game(object):
                 self.lemmings[lemming_index].frame = self.head_frame
                 lemming_index -= 1
 
+    def detatchHeadLemming(self):
+        self.control_lemming += 1
+        self.lemmings[self.control_lemming].sprite.opacity = 255
+        self.head_frame = self.lemmings[self.control_lemming].frame
 
     def update(self, dt):
-        first = self.lemmings[0]
-        self.head_frame = Game.LemmingFrame(Vec2d(first.frame.pos), Vec2d(first.frame.vel), self.head_frame)
+        # detach head lemming from the rest
+        if self.explode_queued:
+            self.explode_queued = False
+            self.detatchHeadLemming()
 
-        for lemming in self.lemmings:
+
+        # lemming trails
+        char = self.lemmings[self.control_lemming]
+        self.head_frame = Game.LemmingFrame(Vec2d(char.frame.pos), Vec2d(char.frame.vel), self.head_frame)
+
+        for lemming in self.lemmings[self.control_lemming:]:
             lemming.frame = lemming.frame.prev_node
             self.updateSpritePos(lemming.sprite, lemming.frame.pos)
         self.lemmings[-1].frame.next_node = None
 
-        # collision with solid blocks
-        new_pos = self.head_frame.pos + self.head_frame.vel * dt
-        block_there = (Vec2d(new_pos.x + tiles.width / 2, new_pos.y) / Game.tile_size).floored()
-        tile_there = self.level.getTile(block_there)
-        if tile_there.solid:
-            new_pos.y = (block_there.y+1)*tiles.height
-            self.head_frame.vel.y = 0
+        for lemming in self.lemmings[:self.control_lemming+1]:
+            if lemming.gone:
+                continue
 
-        # apply velocity to position
-        self.head_frame.pos = new_pos
-        self.updateSpritePos(first.sprite, self.head_frame.pos)
+            frame = lemming.frame
+            # collision with solid blocks
+            new_pos = frame.pos + frame.vel * dt
+            block_there = (Vec2d(new_pos.x + tiles.width / 2, new_pos.y) / Game.tile_size).floored()
+            tile_there = self.level.getTile(block_there)
+            if tile_there.solid:
+                new_pos.y = (block_there.y+1)*tiles.height
+                frame.vel.y = 0
 
-        on_ground = self.tileAt(Vec2d(self.head_frame.pos.x + tiles.width / 2, self.head_frame.pos.y-1)).solid
+            # apply velocity to position
+            frame.pos = new_pos
+            self.updateSpritePos(lemming.sprite, frame.pos)
 
-        # scroll the level
-        self.scroll = Vec2d(self.head_frame.pos)
-        if self.scroll.x < 0:
-            self.scroll.x = 0
-        self.scroll -= Vec2d(self.window.width, self.window.height) / 2
+            on_ground = self.tileAt(Vec2d(frame.pos.x + tiles.width / 2, frame.pos.y-1)).solid
 
-        # apply input to physics
-        acceleration = 500
-        max_speed = 200
-        if self.control_state[Game.Control.MoveLeft]:
-            if self.head_frame.vel.x - acceleration * dt < -max_speed:
-                self.head_frame.vel.x = -max_speed
-            else:
-                self.head_frame.vel.x -= acceleration * dt
-        if self.control_state[Game.Control.MoveRight]:
-            if self.head_frame.vel.x + acceleration * dt > max_speed:
-                self.head_frame.vel.x = max_speed
-            else:
-                self.head_frame.vel.x += acceleration * dt
-        if self.control_state[Game.Control.Jump] and on_ground:
-            jump_velocity = 350
-            self.head_frame.vel.y = jump_velocity
+            if lemming == char:
+                # scroll the level
+                desired_scroll = Vec2d(frame.pos)
+                if desired_scroll.x < 0:
+                    desired_scroll.x = 0
+                desired_scroll -= Vec2d(self.window.width, self.window.height) / 2
+                xdist1 = desired_scroll.x - self.scroll.x
+                ydist1 = desired_scroll.y - self.scroll.y
+                self.scroll.x += xdist1 * Game.target_fps * 0.15 * dt
+                self.scroll.y += ydist1 * Game.target_fps * 0.15 * dt
 
-        # gravity
-        gravity_accel = 800
-        if not on_ground:
-            self.head_frame.vel.y -= gravity_accel * dt
 
-        def sign(n):
-            if n > 0:
-                return 1
-            elif n < 0:
-                return -1
-            else:
-                return 0
-        # friction
-        friction_accel = 150
-        if on_ground:
-            if abs(self.head_frame.vel.x) < abs(friction_accel * dt):
-                self.head_frame.vel.x = 0
-            else:
-                self.head_frame.vel.x += friction_accel * dt * -sign(self.head_frame.vel.x)
+                # apply input to physics
+                acceleration = 500
+                max_speed = 200
+                if self.control_state[Game.Control.MoveLeft]:
+                    if frame.vel.x - acceleration * dt < -max_speed:
+                        frame.vel.x = -max_speed
+                    else:
+                        frame.vel.x -= acceleration * dt
+                if self.control_state[Game.Control.MoveRight]:
+                    if frame.vel.x + acceleration * dt > max_speed:
+                        frame.vel.x = max_speed
+                    else:
+                        frame.vel.x += acceleration * dt
+                if self.control_state[Game.Control.Jump] and on_ground:
+                    jump_velocity = 350
+                    frame.vel.y = jump_velocity
+
+            # gravity
+            gravity_accel = 800
+            if not on_ground:
+                frame.vel.y -= gravity_accel * dt
+
+            # friction
+            friction_accel = 150
+            if on_ground:
+                if abs(frame.vel.x) < abs(friction_accel * dt):
+                    frame.vel.x = 0
+                else:
+                    frame.vel.x += friction_accel * dt * -sign(frame.vel.x)
 
         # prepare sprites for drawing
         start = self.absPt(Vec2d(0, 0)) / Game.tile_size
@@ -200,9 +236,17 @@ class Game(object):
 
     def on_key_press(self, symbol, modifiers):
         try:
-            self.control_state[self.controls[symbol]] = True
+            control = self.controls[symbol]
+            self.control_state[control] = True
         except KeyError:
-            pass
+            return
+        if control == Game.Control.Explode:
+            self.explode_queued = True
+        elif control == Game.Control.BellyFlop:
+            self.bellyflop_queued = True
+        elif control == Game.Control.Freeze:
+            self.freeze_queued = True
+
 
     def on_key_release(self, symbol, modifiers):
         try:
