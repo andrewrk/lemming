10c040b4205dba36f57d15d1472d20826877b27f
Andrew Kelley <superjoe30@gmail.com>
2011-04-05 01:52:02 -0700

    turns out git add -A doesn't work for parent directories.

--- data/tiles.tsx
+++ data/tiles.tsx
@@ -1,6 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="default" tilewidth="16" tileheight="16">
- <image source="tiles.png" width="64" height="32"/>
+ <image source="tiles.png" width="64" height="80"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="DirtRampRight"/>
+   <property name="ramp" value="1"/>
+  </properties>
+ </tile>
  <tile id="1">
   <properties>
    <property name="name" value="PlusOne"/>
@@ -39,6 +45,66 @@
   <properties>
    <property name="mine" value="1"/>
    <property name="name" value="LandMine"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="name" value="DirtRampLeft"/>
+   <property name="ramp" value="-1"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="breakable" value="1"/>
+   <property name="name" value="WeakStone"/>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="name" value="Stone"/>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="name" value="Brick"/>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="name" value="Button"/>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="name" value="ButtonPressed"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="name" value="BrickTop"/>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="name" value="DeadBodyLeft"/>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="name" value="DeadBodyMiddle"/>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="name" value="DeadBodyRight"/>
+   <property name="solid" value="1"/>
   </properties>
  </tile>
 </tileset>
