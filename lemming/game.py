82a6cf95cb910440175edda288517397a765a26c
Andrew Kelley <superjoe30@gmail.com>
2011-04-08 15:51:52 -0700

    fix the lower bound for level file in game

--- lemming/game.py
+++ lemming/game.py
@@ -7,7 +7,7 @@ import levelplayer
 import winscreen
 import os
 
-levels = ['level{0}.tmx'.format(i) for i in range(5)]
+levels = ['level{0}.tmx'.format(i) for i in range(1, 5)]
 
 target_fps = 60
 
